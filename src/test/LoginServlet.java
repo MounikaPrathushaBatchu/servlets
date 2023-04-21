bb4b84ce36d9fe8534f42435972cb19139aca175
Admin <mounikabatchu5@gmail.com>
2023-04-21 17:59:30 +0530

    servlet example

---
+++ src/test/LoginServlet.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException {
+		PrintWriter pw = response.getWriter();
+		response.setContentType("text/html");
+		String uname = request.getParameter("uname");
+		String pword = request.getParameter("pword");
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.10.230:1521:orcl", "training", "training");
+			PreparedStatement ps = con.prepareStatement("select * from Login where uname = ? and pword = ?");
+			ps.setString(1, uname);
+			ps.setString(2, pword);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				pw.println("Login Successful");
+				RequestDispatcher rd = request.getRequestDispatcher("dis");
+				rd.forward(request, response);
+			} else {
+				pw.println("InValid UserLogin");
+				RequestDispatcher rd = request.getRequestDispatcher("Login.html");
+				rd.include(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
